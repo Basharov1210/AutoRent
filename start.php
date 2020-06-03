0d7ccb78b84a31de410c8ef418bbf702cb8e94b2
Basharov1210 <48106815+Basharov1210@users.noreply.github.com>
2020-06-03 18:17:43 +0300

    Update start.php

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
     session_start();
     
 	try {
-		$pdo = new PDO("mysql:dbname=rencar_db;host=127.0.0.1:3307", "root", "");
+		$pdo = new PDO("mysql:dbname=sifwrx5zwg3230rg;host=r42ii9gualwp7i1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306", "hn7md7fkskj92j6h", "plsn9tkvdauotpsf");
 		$pdo->exec("SET NAMES `utf8`");
 		
 	} catch (PDOException $e) {
